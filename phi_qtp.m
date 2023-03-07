c6a700e3830c635d9bbb1cbf6f81a0f6d76e17da
amedinae <31489998+amedinae@users.noreply.github.com>
2023-03-07 10:32:51 +0100

    Initial init

---
+++ phi_qtp.m
@@ -0,0 +1,7 @@
+function x_next = phi_qtp(x, u, options)
+% X_NEXT=PHI_QTP(X, U) compute next state from current state X and control input U.
+%
+% Simulation function in order to compute the next value in the quad tank
+% process. You can optionnally pass a sampling period with the key 'dt' (default
+% value is 1).
+end
